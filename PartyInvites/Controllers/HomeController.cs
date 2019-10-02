01645c468a2a398147d4f91ffd21e443ea9eee1e
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-10-02 16:40:04 +0100

    Added a register form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -34,9 +34,18 @@ namespace PartyInvites.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(GuestResponse response)
+        {
+            //todo: Store the answer
+
+            return View("ThankYou");
         }
     }
 }
