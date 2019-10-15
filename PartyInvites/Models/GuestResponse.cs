87e688c7fe38cfdf7c8acc2f981aeffcbda8cfb1
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-10-15 12:17:55 +0100

    Added validation for email address

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -11,6 +11,7 @@ namespace PartyInvites.Models
         [Required(ErrorMessage ="Please enter your name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter your Email adress")]
+        [EmailAddress]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
